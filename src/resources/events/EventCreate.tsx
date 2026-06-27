ba9d6c41f4457c0f0db3531809bc900de17ad8f1
DavFilsDev <miharisoadavidfils@gmail.com>
2026-06-27 16:59:17 +0300

    feat(events): add EventCreate form

---
+++ src/resources/events/EventCreate.tsx
@@ -0,0 +1,34 @@
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  DateTimeInput,
+  required,
+} from 'react-admin';
+
+export const EventCreate = () => (
+  <Create>
+    <SimpleForm>
+      <TextInput source="title" label="Title" validate={required()} fullWidth />
+      <TextInput
+        source="description"
+        label="Description"
+        multiline
+        rows={4}
+        fullWidth
+      />
+      <TextInput source="location" label="Location" fullWidth />
+      <TextInput source="bannerUrl" label="Banner URL" fullWidth />
+      <DateTimeInput
+        source="startDate"
+        label="Start date"
+        validate={required()}
+      />
+      <DateTimeInput
+        source="endDate"
+        label="End date"
+        validate={required()}
+      />
+    </SimpleForm>
+  </Create>
+);
